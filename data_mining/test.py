42b0fa74c3f21dcd46a315e9e3074892bc0ea9f9
davo12300 <107316298+davo12300@users.noreply.github.com>
2023-06-07 22:43:01 -0600

    Grouped data practice

--- data_mining/test.py
+++ data_mining/test.py
@@ -21,7 +21,7 @@ rango = max(datos_lista) - min(datos_lista)
 
 intervalos  = round(1 + (3.322 * (math.log(len(datos_lista),10))))  
 amplitud = rango/intervalos
-print(intervalos, amplitud)
+
 
 ##LISTAS QUE CONTENDRAN LOS LIMITES Y LA FRECUENCIA DE LOS DATOS
 l_inferior = []
@@ -31,7 +31,7 @@ frec_data = []
 calculo= min(datos_lista)
 for x in range(intervalos):
 
-    print(x)
+    
     l_inferior.append(calculo)
     l_superior.append(calculo+amplitud)
     calculo = calculo + amplitud
@@ -70,20 +70,61 @@ for r in range(len(m_clase)):
     fi_xi.append(operation)
     suma_fi = suma_fi + operation
 
+##MEDIANA
+
+dato = len(datos_lista)/2
+
+frame = []
+for x in range(len(fi_data)):
+    op = dato - fi_data[x]
+    if op > 0:
+        frame.append(op)
+    else:
+        frame.append(op*-1)
+print(min(frame))
+indice = 0
+for c in range(len(frame)):   
+    if frame[c] == min(frame):
+        indice = c
+        break
+
+calculo_mediana = l_inferior[indice] + ( ((len(datos_lista)/2) - (fi_data[indice-1]))/ frec_data[indice] ) * (l_inferior[indice]-l_superior[indice])
+
+
+#MODA
+
+position = 0 
+convert = frec_data
+for c in range(len(convert)):   
+    if convert[c] == max(convert):
+        position = c
+        break
+if position == 0:
+    bug = convert[0]
+    convert[0] = 0
+    print(max(convert))
+    position = max(convert)
+    for d in range(len(frec_data)):
+        if convert[d] == max(convert):
+            position = d
+            convert[0] = bug 
+            break
+p = position
+operacion_moda = (l_inferior[p]) + (( frec_data[p] - frec_data[p-1] )/(  ( frec_data[p] - frec_data[p-1] ) + (frec_data[p] - frec_data[p+1] ) ))
 
 
 
 
 ##IMPRESION DE RESULTADOS
 
-print('INTERVALOS \t| fi\t| FI\t| xi\t| fr\t| fi*xi\t|')
+print('INTERVALOS \t| fi\t| FI\t| xi\t| fr\t| fi*xi\t')
 for q in range(len(frec_data)):
-    print(round(l_inferior[q],2) ,'-', round(l_superior[q] ,2),'\t|' ,frec_data[q], '\t|',fi_data[q], '\t|', m_clase[q],'\t|',frec_relativa[q],'\t|',fi_xi[q],'\t|')
+    print(round(l_inferior[q],2) ,'-', round(l_superior[q] ,2),'\t|' ,frec_data[q],'\t|',fi_data[q], '\t|', m_clase[q],'\t|',frec_relativa[q],'\t|',fi_xi[q])
 
 print('S:\t' , round(suma_fi,3))
 print('MEDIA:\t', round( (suma_fi/len(datos_lista)),3))
-print('MEDINA:\t')
-print('MODA:\t')
+print('MEDINA:\t', round(calculo_mediana,2))
+print('MODA:\t', round(operacion_moda,2))
 
 
     
